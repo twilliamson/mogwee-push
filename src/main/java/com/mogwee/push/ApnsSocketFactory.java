e704dde5db9e0fefe3ee63efd99a84cd76d8f24e
Tim Williamson <tim@ning.com>
2012-01-31 11:05:52 -0800

    add constructor that takes an SSLContext (to allow full control over SSL socket creation)

--- src/main/java/com/mogwee/push/ApnsSocketFactory.java
+++ src/main/java/com/mogwee/push/ApnsSocketFactory.java
@@ -43,7 +43,29 @@ public class ApnsSocketFactory
     private final int port;
     private final SSLSocketFactory socketFactory;
 
+    /**
+     * Creates a factory for creating sockets to send push notifications.
+     *
+     * @param keystore         path to keystore file
+     * @param keystorePassword password for keystore
+     * @param keystoreType     type of keystore, e.g., JKS
+     * @param type             whether socket is for production or development
+     * @throws GeneralSecurityException if there's an error initializing the keystore or TLS
+     * @throws IOException              if there's an error opening the keystore or creating the socket
+     */
     public ApnsSocketFactory(String keystore, String keystorePassword, String keystoreType, Type type) throws GeneralSecurityException, IOException
+    {
+        this(createContext(new File(keystore), keystorePassword, keystoreType), type);
+    }
+
+    /**
+     * Creates a factory for creating sockets to send push notifications.
+     *
+     * @param context context used to create the underlying socket
+     * @param type    whether socket is for production or development
+     * @throws IOException if there's an error creating the socket
+     */
+    public ApnsSocketFactory(SSLContext context, Type type) throws IOException
     {
         this.type = type;
 
@@ -77,29 +99,6 @@ public class ApnsSocketFactory
                 throw new IllegalArgumentException("Unknown type: " + type);
         }
 
-        TrustManagerFactory trustManagerFactory = TrustManagerFactory.getInstance("sunx509");
-        KeyStore appleStore = KeyStore.getInstance("JKS");
-        InputStream appleStoreInputStream = null;
-
-        try {
-            // created by com.mogwee.push.CreateAppleCertificateKeystore (in tests)
-            appleStoreInputStream = getClass().getResourceAsStream("/apple.keystore");
-            appleStore.load(appleStoreInputStream, "apple".toCharArray());
-        }
-        finally {
-            CloseableUtil.closeQuietly(appleStoreInputStream);
-        }
-
-        trustManagerFactory.init(appleStore);
-
-        KeyManagerFactory keyManagerFactory = KeyManagerFactory.getInstance("sunx509");
-        SSLContext context = SSLContext.getInstance("TLS");
-        char[] password = keystorePassword.toCharArray();
-        KeyStore.ProtectionParameter passwordProtection = new KeyStore.PasswordProtection(password);
-        KeyStore keyStore = KeyStore.Builder.newInstance(keystoreType, null, new File(keystore), passwordProtection).getKeyStore();
-
-        keyManagerFactory.init(keyStore, password);
-        context.init(keyManagerFactory.getKeyManagers(), trustManagerFactory.getTrustManagers(), null);
         this.socketFactory = context.getSocketFactory();
     }
 
@@ -112,5 +111,34 @@ public class ApnsSocketFactory
     public String toString()
     {
         return "ApnsSocketFactory_" + type;
+    }
+
+    private static SSLContext createContext(File keystore, String keystorePassword, String keystoreType) throws GeneralSecurityException, IOException
+    {
+        TrustManagerFactory trustManagerFactory = TrustManagerFactory.getInstance("sunx509");
+        KeyStore appleStore = KeyStore.getInstance("JKS");
+        InputStream appleStoreInputStream = null;
+
+        try {
+            // created by com.mogwee.push.CreateAppleCertificateKeystore (in tests)
+            appleStoreInputStream = ApnsSocketFactory.class.getResourceAsStream("/apple.keystore");
+            appleStore.load(appleStoreInputStream, "apple".toCharArray());
+        }
+        finally {
+            CloseableUtil.closeQuietly(appleStoreInputStream);
+        }
+
+        trustManagerFactory.init(appleStore);
+
+        KeyManagerFactory keyManagerFactory = KeyManagerFactory.getInstance("sunx509");
+        SSLContext context = SSLContext.getInstance("TLS");
+        char[] password = keystorePassword.toCharArray();
+        KeyStore.ProtectionParameter passwordProtection = new KeyStore.PasswordProtection(password);
+        KeyStore keyStore = KeyStore.Builder.newInstance(keystoreType, null, keystore, passwordProtection).getKeyStore();
+
+        keyManagerFactory.init(keyStore, password);
+        context.init(keyManagerFactory.getKeyManagers(), trustManagerFactory.getTrustManagers(), null);
+
+        return context;
     }
 }
